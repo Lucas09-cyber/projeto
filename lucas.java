2c16dddd3f985288b558ab6cdb782d1f9d1bf5e1
Lucas vie <lucasvierobotica@gmail.com>
2026-05-17 19:36:08 -0300

    criado metodo main

---
+++ lucas.java
@@ -0,0 +1,9 @@
+public class lucas {
+    public static void main(String[] args) {
+        apresentaOla();
+    }
+    
+    public static void apresentaOla() {
+        System.out.println("ola");
+    }
+}

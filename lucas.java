00e671319db17f8e9718520fe9f4b521d4d7b939
Lucas vie <lucasvierobotica@gmail.com>
2026-05-17 19:40:19 -0300

    java deletado

--- lucas.java
+++
@@ -1,9 +0,0 @@
-public class lucas {
-    public static void main(String[] args) {
-        apresentaOla();
-    }
-    
-    public static void apresentaOla() {
-        System.out.println("ola");
-    }
-}
